9c84e3d6a2202e07643c7d2f0828a708e33eb52a
Benjamin Van Renterghem <42121860+benjaminvanrenterghem@users.noreply.github.com>
2022-07-21 04:33:53 +0200

    Update JwtTokenParser.cs

--- Micro2Go/Parsers/JwtTokenParser.cs
+++ Micro2Go/Parsers/JwtTokenParser.cs
@@ -1,25 +1,88 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
 using System.Net.Http.Headers;
+using System.Security.Claims;
 using Micro2Go.Model;
 using Microsoft.AspNetCore.Http;
 
 namespace Micro2Go.Parsers {
 	public static class JwtTokenParser {
-		public static ParsedJwtToken ParseRequest(HttpRequest request) {
+		private static List<Claim>? ProvideClaims(string jwtToken) {
 			try {
-				var claims = new JwtSecurityTokenHandler().ReadJwtToken(request.Headers["Authorization"].ToString().Replace("Bearer ", "")).Claims;
+				return new JwtSecurityTokenHandler().ReadJwtToken(jwtToken.Replace("Bearer ", ""))
+													.Claims
+													.ToList();
+			} catch (Exception e) when(e.GetType() == typeof(ArgumentException)
+									|| e.GetType() == typeof(ArgumentNullException)) {
+			return null;
+		}
+	}
 
-				return new ParsedJwtToken() {
-					UserId = int.Parse(claims?.FirstOrDefault(claim => claim.Type.ToLower() == "userid")?.Value ?? "-1"),
-					Email = claims?.FirstOrDefault(claim => claim.Type.ToLower() == "email")?.Value ?? "",
-					DisplayName = claims?.FirstOrDefault(claim => claim.Type.ToLower() == "displayname")?.Value ?? "",
-					LoginName = claims?.FirstOrDefault(claim => claim.Type.ToLower() == "loginname")?.Value ?? "",
-					ClearanceLevels = claims?.Where(claim => claim.Type.ToLower() == "clearancelevel")?.Select(claim => Enum.Parse<ClearanceLevel>(claim.Value.ToString()))?.ToList() ?? new(),
-				};
-			} catch (Exception e) when (e.GetType() == typeof(ArgumentException)
-									 || e.GetType() == typeof(ArgumentNullException)) {
+		private static ParsedJwtToken? ParseJwtToken(string jwtToken) {
+			var claims = JwtTokenParser.ProvideClaims(jwtToken);
+
+			if(claims is null) {
 				return null;
 			}
+
+			return new ParsedJwtToken() {
+				UserId = int.Parse(claims?.FirstOrDefault(claim => claim.Type.ToLower() == "userid")?.Value ?? "-1"),
+				Email = claims?.FirstOrDefault(claim => claim.Type.ToLower() == "email")?.Value ?? "",
+				DisplayName = claims?.FirstOrDefault(claim => claim.Type.ToLower() == "displayname")?.Value ?? "",
+				LoginName = claims?.FirstOrDefault(claim => claim.Type.ToLower() == "loginname")?.Value ?? "",
+				ClearanceLevels = claims?.FirstOrDefault(claim => claim.Type == nameof(ClearanceLevel))
+										?.Value.Split(",")
+										?.ToList()
+										?.ConvertAll(clstr => Enum.Parse<ClearanceLevel>(clstr))
+										?? new()
+			};
+		}
+
+		public static ParsedJwtToken ParseBareToken(string jwtToken) {
+			return JwtTokenParser.ParseJwtToken(jwtToken);
+		}
+
+		public static ParsedJwtToken ParseRequest(HttpRequest request) {
+			return JwtTokenParser.ParseJwtToken(request.Headers["Authorization"].ToString());
+		}
+
+		// Ten behoeve van unit tests & validatie qua inhoud
+		public static VerboseParsedJwtToken? ParseAndProvideVerboseJwtToken(string jwtToken) { 
+			List<Claim> claims = JwtTokenParser.ProvideClaims(jwtToken);
+			ParsedJwtToken standardJwt = JwtTokenParser.ParseBareToken(jwtToken);
+
+			if(standardJwt is null){
+				return null;
+			}
+
+			return new VerboseParsedJwtToken {
+				UserId = standardJwt.UserId,
+				Email = standardJwt.Email,
+				DisplayName = standardJwt.DisplayName,
+				LoginName = standardJwt.LoginName,
+				ClearanceLevels = standardJwt.ClearanceLevels,
+
+				Issuer = claims.Where(cl => cl.Type.ToLower() == "iss")
+							   .FirstOrDefault()
+							   ?.Value
+							   ?? "",
+				Audience = claims.Where(cl => cl.Type.ToLower() == "aud")
+							   .FirstOrDefault()
+							   ?.Value
+							   ?? "",
+				Subject = claims.Where(cl => cl.Type.ToLower() == "sub")
+							   .FirstOrDefault()
+							   ?.Value
+							   ?? "",
+				Expiration = DateTimeOffset.FromUnixTimeMilliseconds(
+					long.Parse(
+						claims.Where(cl => cl.Type.ToLower() == "exp")
+								.FirstOrDefault()
+								?.Value
+								?? "0"
+					) * 1000
+				).DateTime
+			};
+
 
 		}
 	}
